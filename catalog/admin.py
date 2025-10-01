13bba99bee899828a2e8548e6999e1b5c9d5ecd1
Aleksandra Surkova <a.surkova90@mail.ru>
2025-10-01 12:37:38 +0300

    HomeWork 23

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Product, Category
+@admin.register(Category)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'category_name',)
+    search_fields = ('category_name', 'category_description',)
 
-# Register your models here.
+@admin.register(Product)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product_name', 'price', 'category',)
+    list_filter = ('category',)
+    search_fields = ('product_name', 'product_description',)
---
+++ catalog/management/commands/add_catalog.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand
+from catalog.models import Category, Product
+
+
+class Command(BaseCommand):
+    help = 'Add test books to the database'
+
+    def handle(self, *args, **kwargs):
+        category, _ = Category.objects.get_or_create(category_name='Ноутбуки', category_description='Ноутбуки')
+
+        products = [
+            {'product_name': 'HUAWEI MateBook D16', 'product_description': 'D16 2024/16"/Core i5-13420H/16/512/Win/Space Gray', 'category': category, 'price': '62000'},
+            {'product_name': 'Apple MacBook', 'product_description': 'Air 13 M1 8/256 Space Gray', 'category': category, 'price': '60000'},
+        ]
+
+        for product_data in products:
+            products, created = Product.objects.get_or_create(**product_data)
+            if created:
+                self.stdout.write(self.style.SUCCESS(f'Successfully added product: {products.product_name}'))
+            else:
+                self.stdout.write(self.style.WARNING(f'Product already exists: {products.product_name}'))
