a143f695b7b7b3b2fbe702104404571b721f28d4
Aleksandra Surkova <a.surkova90@mail.ru>
2025-10-17 09:19:40 +0300

    refactoring refactoring - catalog/forms.py: validation of forms for prohibited words, price field, styling forms refactoring - blog/forms.py: validation of forms for prohibited words, price field, styling forms

---
+++ catalog/forms.py
@@ -0,0 +1,45 @@
+from django.core.exceptions import ValidationError
+from django.db.models import BooleanField
+from django.forms import ModelForm, forms
+
+from catalog.models import Product
+
+
+class ProductForm(ModelForm):
+    class Meta:
+        model = Product
+        exclude = ('created_at', 'updated_at')
+
+    words = [
+        "казино", "криптовалюта", "крипта", "биржа",
+        "дешево", "бесплатно", "обман", "полиция", "радар"
+    ]
+
+    def clean_product_name(self):
+        product_name = self.cleaned_data.get('product_name')
+        for word in self.words:
+            if word.lower() in product_name.lower():
+                raise ValidationError(f'Слово {word} нельзя использовать')
+        return product_name
+
+    def clean_product_description(self):
+        product_description = self.cleaned_data.get('product_description')
+        for word in self.words:
+            if word.lower() in product_description.lower():
+                raise ValidationError(f'Слово {word} нельзя использовать')
+        return product_description
+
+    def clean_price(self):
+        price = self.cleaned_data.get("price")
+        if price is not None and price < 0:
+            raise forms.ValidationError("Цена не может быть отрицательной")
+        return price
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({
+                "class": "form-control",
+                "placeholder": field.label
+            })
+            
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from . import views
-from .views import ProductListView, ProductDetailView, ContactsView
+from .views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, \
+    ProductDeleteView, ContactsView
 
 app_name = 'catalog'
 
 urlpatterns = [
     path("", ProductListView.as_view(), name="home"),
-    path("contacts/", ContactsView.as_view(), name="contacts"),
     path("product_detail/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
+    path("product/create/", ProductCreateView.as_view(), name="product_create"),
+    path("product/<int:pk>/update/", ProductUpdateView.as_view(), name="product_update"),
+    path("product/<int:pk>/delete/", ProductDeleteView.as_view(), name="product_delete"),
+    path("contacts/", ContactsView.as_view(), name="contacts"),
 ]
